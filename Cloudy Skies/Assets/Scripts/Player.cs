525316be03c86c5f9951879f3b29adbc2270238d
Mark <1007004@student.sae.edu.au>
2018-08-08 20:26:25 +1000

    Collisions, Movement and Jumping

--- Cloudy Skies/Assets/Scripts/Player.cs
+++ Cloudy Skies/Assets/Scripts/Player.cs
@@ -7,8 +7,38 @@ public class Player : MonoBehaviour
 {
     Controller2D controller;
 
+    Vector3 velocity;
+    float moveSpeed = 6;
+    float gravity;
+    float jumpVelocity;
+
+    float jumpHeight = 4;
+    float timeToJumpApex = .4f;
+
 	void Start()
     {
         controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight / Mathf.Pow(timeToJumpApex, 2));
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    void Update()
+    {
+        if (controller.collisionInfo.above || controller.collisionInfo.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisionInfo.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+
+        velocity.x = input.x * moveSpeed;
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }

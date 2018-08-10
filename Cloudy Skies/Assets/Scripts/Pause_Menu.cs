94029ed9f88ba7dde6b90c9ba50693b79394bb0a
Mark <1007004@student.sae.edu.au>
2018-08-10 14:53:06 +1000

    Fixing the Pause Menu

--- Cloudy Skies/Assets/Scripts/Pause_Menu.cs
+++ Cloudy Skies/Assets/Scripts/Pause_Menu.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class Pause_Menu : MonoBehaviour
 {
     [Header("Scene Management")]
-    //public string scene;
+    public string scene;
 
     [Header("Player Settings")]
 
@@ -35,7 +35,8 @@ public class Pause_Menu : MonoBehaviour
 
     public void Pause()
     {
-        if (pauseMenu.gameObject.activeInHierarchy == false && Input.GetKeyDown(KeyCode.P))
+        //if (pauseMenu.gameObject.activeInHierarchy == false && Input.GetKeyDown(KeyCode.P))
+        if (pauseMenu.gameObject.activeInHierarchy == false)
         {
             Time.timeScale = 0;
             pauseMenu.gameObject.SetActive(true);
@@ -50,7 +51,8 @@ public class Pause_Menu : MonoBehaviour
             pauseOptionsMenu.gameObject.SetActive(false);
         }
 
-        else if (pauseMenu.gameObject.activeInHierarchy == true && Input.GetKeyDown(KeyCode.Escape))
+        //else if (pauseMenu.gameObject.activeInHierarchy == true && Input.GetKeyDown(KeyCode.Escape))
+        else if (pauseMenu.gameObject.activeInHierarchy == true)
         {
             pauseMenu.gameObject.SetActive(false);
             Time.timeScale = 1;
@@ -74,26 +76,114 @@ public class Pause_Menu : MonoBehaviour
             pauseStatusMenu.gameObject.SetActive(false);
             pauseOptionsMenu.gameObject.SetActive(false);
         }
+
+        else
+        {
+            pauseItemsMenu.gameObject.SetActive(false);
+
+            UnityEngine.Cursor.visible = true;
+        }
     }
 
     public void ToggleEquipMenu()
     {
+        if (pauseEquipMenu.gameObject.activeInHierarchy == false)
+        {
+            pauseEquipMenu.gameObject.SetActive(true);
+            //Set the time to 0 - No run time && set Pause_Menu to true
 
+            UnityEngine.Cursor.visible = true;
+
+            pauseMenu.gameObject.SetActive(false);
+            pauseItemsMenu.gameObject.SetActive(false);
+            pauseSkillsMenu.gameObject.SetActive(false);
+            pauseStatusMenu.gameObject.SetActive(false);
+            pauseOptionsMenu.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            pauseEquipMenu.gameObject.SetActive(false);
+
+            UnityEngine.Cursor.visible = true;
+        }
     }
 
     public void ToggleSkillsMenu()
     {
+        if (pauseSkillsMenu.gameObject.activeInHierarchy == false)
+        {
+            pauseSkillsMenu.gameObject.SetActive(true);
+            //Set the time to 0 - No run time && set Pause_Menu to true
 
+            UnityEngine.Cursor.visible = true;
+
+            pauseMenu.gameObject.SetActive(false);
+            pauseItemsMenu.gameObject.SetActive(false);
+            pauseEquipMenu.gameObject.SetActive(false);
+            pauseStatusMenu.gameObject.SetActive(false);
+            pauseOptionsMenu.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            pauseSkillsMenu.gameObject.SetActive(false);
+
+            UnityEngine.Cursor.visible = true;
+        }
     }
 
     public void ToggleStatusMenu()
     {
+        if (pauseStatusMenu.gameObject.activeInHierarchy == false)
+        {
+            pauseStatusMenu.gameObject.SetActive(true);
+            //Set the time to 0 - No run time && set Pause_Menu to true
 
+            UnityEngine.Cursor.visible = true;
+
+            pauseMenu.gameObject.SetActive(false);
+            pauseItemsMenu.gameObject.SetActive(false);
+            pauseEquipMenu.gameObject.SetActive(false);
+            pauseSkillsMenu.gameObject.SetActive(false);
+            pauseOptionsMenu.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            pauseStatusMenu.gameObject.SetActive(false);
+
+            UnityEngine.Cursor.visible = true;
+        }
     }
 
     public void ToggleOptionsMenu()
     {
+        if (pauseOptionsMenu.gameObject.activeInHierarchy == false)
+        {
+            pauseOptionsMenu.gameObject.SetActive(true);
+            //Set the time to 0 - No run time && set Pause_Menu to true
 
+            UnityEngine.Cursor.visible = true;
+
+            pauseMenu.gameObject.SetActive(false);
+            pauseItemsMenu.gameObject.SetActive(false);
+            pauseEquipMenu.gameObject.SetActive(false);
+            pauseSkillsMenu.gameObject.SetActive(false);
+            pauseStatusMenu.gameObject.SetActive(false);
+        }
+
+        else
+        {
+            pauseOptionsMenu.gameObject.SetActive(false);
+
+            UnityEngine.Cursor.visible = true;
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(scene);
     }
 
     public void QuitGame()

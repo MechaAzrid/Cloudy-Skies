1c30fc225d184886fc6b34c1eb10aed4484b2ea7
Mark Singca <mark.singca975@gmail.com>
2018-08-17 10:32:21 +1000

    Adding in Player UI, Player Health Manager and Generic Box Enemy Scripts

---
+++ Cloudy Skies/Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthManager : MonoBehaviour 
+{
+    [Header("Player Variables")]
+    public int playerMaxHealth;
+    public int playerCurrHealth;
+
+	void Start () 
+    {
+        //playerCurrHealth = playerMaxHealth;
+        playerCurrHealth = Mathf.Clamp(playerCurrHealth, 0, playerMaxHealth);
+	}
+	
+	void Update () 
+    {
+		if (playerCurrHealth <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void DamagePlayer(int damageToGive)
+    {
+        playerCurrHealth -= damageToGive;
+    }
+
+    public void MaxHealth()
+    {
+        playerCurrHealth = playerMaxHealth;
+    }
+}
---
+++ Cloudy Skies/Assets/Scripts/Prototype/BoxEnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxEnemyController : MonoBehaviour 
+{
+    public int damageToGive;
+	void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealthManager>().DamagePlayer(damageToGive);
+        }
+    }
+}
